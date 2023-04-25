4e9268b28cedb61d349c135fe4d6df6549bc10db
Matthew Pacey <paceym@oregonstate.edu>
2023-04-24 22:20:56 -0700

    Cleaned up and added comments

--- hw1/main.py
+++ hw1/main.py
@@ -3,14 +3,18 @@
 import numpy as np
 import random
 
-# AI 533 - HW1 - Gridworld
+# AI 533 - HW1 - Gridworld with value iteration and policy iteration
 # Matthew Pacey
 
-# set
+# format the output of numpy array printing
 np.set_printoptions(precision=4)
+np.set_printoptions(suppress=True)
 
-# if true, gridworld values printed at each timestep; if false only final values printed
+# my debug helper: if true, gridworld values printed at each timestep; if false only final values printed
 print_intermediate = False
+
+# maximum number of iterations in each convergence (not really a concern in this scenario)
+MAX_ITER = 1000
 
 def get_start_state():
     """
@@ -19,16 +23,21 @@ def get_start_state():
     """
     util = np.zeros((4, 4))
     util[3, 3] = 100  # goal state, +100 reward
-    # util[0, 1] = -10  # fire, -10 reward
-    # util[0, 2] = -10  # fire, -10 reward
-    # util[2, 1] = -5  # water, -5 reward
-    # util[2, 2] = -5  # water, -5 reward
     return util
 
+
 def get_val(util, x, y):
+    """
+    Get the value of the requested cell, return None if it is outside the grid
+    :param util:
+    :param x:
+    :param y:
+    :return:
+    """
     if x < 0 or y < 0 or x > len(util) - 1 or y > len(util[0]) - 1:
         return None
     return util[x, y]
+
 
 def print_path(util):
     """
@@ -47,7 +56,6 @@ def print_path(util):
             lt = get_val(util, x, y - 1)
             up = get_val(util, x - 1, y)
             dn = get_val(util, x + 1, y)
-            # print(f"{rt=}, {lt=}, {up=}, {dn=}")
 
             # default behavior: stay in cell if no neighbor has higher val
             policy_dir[x, y] = 'o'
@@ -80,10 +88,8 @@ def get_updated_value(x, y, util, gamma):
     if (x, y) == (2, 1) or (x, y) == (2, 2):
         return -5
 
-    # TODO : what happens if the agent cannot move (i.e. edge cell)
-    #   does that mean the value is 0 or the value of the current cell?
-
-    cell = -1 # util[x, y]
+    # reward of negative 1 for each movement
+    cell = -1
     if y < len(util[0]) - 1:
         val_rt = util[x, y + 1]
     else:
@@ -101,44 +107,33 @@ def get_updated_value(x, y, util, gamma):
     else:
         val_lt = cell
 
-    if x == 0 and y == 2:
-        pass
-
     move_rt = 0.8 * (cell + gamma * val_rt) + 0.1 * (cell + gamma * val_up) + 0.1 * (cell + gamma * val_dn)
     move_lt = 0.8 * (cell + gamma * val_lt) + 0.1 * (cell + gamma * val_up) + 0.1 * (cell + gamma * val_dn)
     move_up = 0.8 * (cell + gamma * val_up) + 0.1 * (cell + gamma * val_lt) + 0.1 * (cell + gamma * val_rt)
     move_dn = 0.8 * (cell + gamma * val_dn) + 0.1 * (cell + gamma * val_lt) + 0.1 * (cell + gamma * val_rt)
     return max(move_rt, move_lt, move_up, move_dn)
 
+
 def value_iteration(max_error, gamma):
     print("\n***************************************************************************")
-    print(f"Value iteration for gamma of {gamma} ({max_error=})")
+    print(f"Value iteration for gamma of {gamma} ")
+
     # initial value of all states
     util = get_start_state()
-    # max change of any state in each loop
-
-    print(f"init state")
-    print(util)
-
+    # convergence criteria: if all value changes are below this value, exit
     threshold = max_error * (1 - gamma) / gamma
 
-    for t in range(500):
+    for t in range(MAX_ITER):
         next_util = np.copy(util)
+        # max change of any state in each loop
         max_change = 0
 
         for x in range(4):
             for y in range(4):
-                # print(f"{next_util[x, y]}")
-                # move right only now
-                # next_util[x, y] = 0.8*(util[x, y] + gamma * util[x, y + 1]) # TODO add 0.1 and 0.1 for other
                 next_util[x, y] = get_updated_value(x, y, util, gamma)
                 change = abs(next_util[x, y] - util[x, y])
                 max_change = max(max_change, change)
 
-        # next_util[0, 2] = 0.72
-        # for idx, x in np.ndenumerate(next_util):
-        #     next_util[idx] = 0.8*(x + gamma * )
-        #     pass
         if print_intermediate:
             print(f"\nAfter time step {t}")
             print(next_util)
@@ -149,7 +144,7 @@ def value_iteration(max_error, gamma):
         if max_change <= threshold:
             break
 
-    print(f"\nFinal state for gamma of {gamma} ({max_error=}, total iterations={t}")
+    print(f"\nFinal state for gamma of {gamma} ({max_error=}, total iterations={t})")
     print(util)
     print("***************************************************************************\n")
 
@@ -170,6 +165,7 @@ def get_random_policy():
 
     return policy
 
+
 def are_policies_the_same(policy_1, policy_2):
     """
     Returns true iff the two policies are the same
@@ -188,6 +184,8 @@ def are_policies_the_same(policy_1, policy_2):
 def get_updated_value_from_policy(x, y, util, gamma, policy):
     """
     Given a policy, compute the util value for the given x,y cell
+    """
+    """
     :param util:
     :param gamma:
     :return:
@@ -245,29 +243,26 @@ def get_dir_of_max_neighbor(x, y, util):
     :return:
     """
     max = -100
-    if y < len(util[0]) - 1:
-        val_rt = util[x, y + 1]
-        if val_rt > max:
-            dir = '>'
-            max = val_rt
-    if x > 0:
-        val_up = util[x - 1, y]
-        if val_up > max:
-            dir = '^'
-            max = val_up
-    if x < len(util) - 1:
-        val_dn = util[x + 1, y]
-        if val_dn > max:
-            dir = 'v'
-            max = val_dn
-    if y > 0:
-        val_lt = util[x, y - 1]
-        if val_lt > max:
-            dir = '<'
-            max = val_lt
+
+    val_rt = get_val(util, x, y + 1)
+    val_lt = get_val(util, x, y - 1)
+    val_up = get_val(util, x - 1, y)
+    val_dn = get_val(util, x + 1, y)
+
+    if val_rt and val_rt > max:
+        dir = '>'
+        max = val_rt
+    if val_up and val_up > max:
+        dir = '^'
+        max = val_up
+    if val_dn and val_dn > max:
+        dir = 'v'
+        max = val_dn
+    if val_lt and val_lt > max:
+        dir = '<'
+        max = val_lt
 
     return dir
-
 
 
 def policy_iteration(max_error, gamma):
@@ -284,62 +279,70 @@ def policy_iteration(max_error, gamma):
 
     # init random policy
     policy = get_random_policy()
-
+    # threshold for considering value matrices equivalent
     threshold = max_error * (1 - gamma) / gamma
-
-    # repeat until no change in policy
+    # number of iterations in value convergence
     iter_cnt = 0
 
+    # repeat outer loop until no change in policy
     while True:
-        next_util = np.copy(util)
-        max_change = 0
 
-        # repeat until converged
-        for x in range(4):
-            for y in range(4):
-                # print(f"{next_util[x, y]}")
-                # move right only now
-                # next_util[x, y] = 0.8*(util[x, y] + gamma * util[x, y + 1]) # TODO add 0.1 and 0.1 for other
-                next_util[x, y] = get_updated_value_from_policy(x, y, util, gamma, policy)
-                change = abs(next_util[x, y] - util[x, y])
-                max_change = max(max_change, change)
-        iter_cnt += 1
+        # repeat this inner loop until the util values converge
+        while True:
+            next_util = np.copy(util)
+            max_change = 0
 
-        if print_intermediate:
-            # print(f"\nAfter time step {t}")
-            print(next_util)
-            print(f"{max_change=}, {threshold=}")
+            for x in range(4):
+                for y in range(4):
+                    # update the util value using the current policy (not best move like VI)
+                    next_util[x, y] = get_updated_value_from_policy(x, y, util, gamma, policy)
+                    change = abs(next_util[x, y] - util[x, y])
+                    max_change = max(max_change, change)
+            iter_cnt += 1
 
-        util = next_util
-        threshold = max_error * (1 - gamma) / gamma
-        if max_change <= threshold:
-            break
+            # debug
+            if print_intermediate:
+                print(next_util)
+                print(f"{iter_cnt=}, {max_change=}, {threshold=}")
 
-            # for each state: compute value
+            util = next_util
+            # break out when util val converges
+            if max_change <= threshold:
+                break
 
-    # for each state in s
-        # update the policy to move to the state with the highest reward
+        # for each state, update the policy to move to the state with the highest reward
         new_policy = np.copy(policy)
         for x in range(4):
             for y in range(4):
-                # TODO
                 new_policy[x, y] = get_dir_of_max_neighbor(x, y, next_util)
 
+        if print_intermediate:
+            print(new_policy)
+            print(f"{max_change=}, {threshold=}")
+
+        # if there was no change in the policy, we can exit the outer loop
         if are_policies_the_same(policy, new_policy):
             break
 
-    print(f"\nFinal state for gamma of {gamma} ({max_error=}, total iterations={iter_cnt}")
+        policy = np.copy(new_policy)
+
+    print(f"\nFinal state for gamma of {gamma} ({max_error=}, total iterations={iter_cnt})")
     print(util)
     print("***************************************************************************\n")
 
     return util, policy
 
 def main():
-    util = value_iteration(max_error=0.001, gamma=0.3)
+    """
+    Main func: run different flavors of VI and PI (will be compared in report)
+    :return:
+    """
+    max_error = 0.0001
+    util = value_iteration(max_error=max_error, gamma=0.3)
     print_path(util)
-    util = value_iteration(max_error=0.001, gamma=0.95)
+    util = value_iteration(max_error=max_error, gamma=0.95)
     print_path(util)
-    util, policy = policy_iteration(max_error=0.001, gamma=0.95)
+    util, policy = policy_iteration(max_error=max_error, gamma=0.95)
     print_path(util)
     print("DONE")
 
